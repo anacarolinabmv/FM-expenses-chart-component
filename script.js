42782a9505777607ee3c44f2fbde2f229bb47af9
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-09-08 12:32:51 +0200

    Refactor initChart function

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-const jsonData = [
-  { day: 'mon', amount: 17.45 },
-  { day: 'tue', amount: 34.91 },
-  { day: 'wed', amount: 52.36 },
-  { day: 'thu', amount: 31.07 },
-  { day: 'fri', amount: 23.39 },
-  { day: 'sat', amount: 43.28 },
-  { day: 'sun', amount: 25.48 },
-];
-
 const changeBckgColor = function (day, dayToday, alpha) {
   const color = day === dayToday ? `rgba(118, 181, 188,${alpha})` : `rgba(236, 119, 95, ${alpha})`;
   day.style.backgroundColor = `${color}`;
 };
 
-const renderChartBars = function () {
+const renderChartBars = function (data) {
   const days = Array.from(document.querySelectorAll('.day'));
 
   const today = new Date().getDay() - 1;
@@ -21,7 +11,7 @@ const renderChartBars = function () {
   dayToday.style.backgroundColor = `rgba(118, 181, 188)`;
 
   days.forEach((day, i) => {
-    day.style.height = `${jsonData[i].amount / 4}rem`;
+    day.style.height = `${data[i].amount / 4}rem`;
 
     day.addEventListener('mouseenter', () => {
       changeBckgColor(day, dayToday, 0.5);
@@ -33,17 +23,19 @@ const renderChartBars = function () {
   });
 };
 
-const initChart = function () {
+const initChart = async function () {
   const chart = document.querySelector('.chart');
+  const response = await fetch('data.json');
+  const data = await response.json();
 
-  jsonData.forEach((data) => {
+  data.forEach((data) => {
     const html = `<div class="day"><p>${data.day}</p>
     <div class="label">$${data.amount}</div>
     </div>`;
     chart.insertAdjacentHTML('beforeend', html);
   });
 
-  renderChartBars();
+  renderChartBars(data);
 };
 
 initChart();
